95b27f863abaf0dcefbbd748f60c0dbb9e20d3cf
Abhinav Chaturvedi <cabhinav774@gmail.com>
2025-12-16 05:46:39 +0530

    learned datatypes in JS

---
+++ 02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";
+// treats all JS code as neer version (cheak gpt)^
+
+// alert(3+3)--> we are using node js, not browser
+// for node js it has different syntax
+
+console.log(3+3);
+console .log("abhinav");
+// code readability should be high
+
+// Datatypes 
+
+let name = "Abhinav"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null =>standalone value
+let state = null;
+// undefined => value not assigned till now
+let password;
+// symbol => unique
+// object* => see letter
+
+// typeof 
+console.log(typeof "abhinav")
+
+console.log (typeof undefined ); // undefined 
+ console.log (typeof null);
+
---
+++ 03_Datatype_conversion.js
@@ -0,0 +1,27 @@
+// let score = "45abc";
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); //NaN => not a number "45abc"
+
+// "45abc"->NaN
+//null -> 0
+//undefined -> Nan
+// true ->1
+// string ->NaN
+
+// let isLoggedIn = 22;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log (booleanIsLoggedIn); 
+
+// "" => false
+// 1=>true , 0=>false 
+// "Abhinav" => true
+// 22 => true
+
+let anyNumber = 56;
+let stringNumber = String (anyNumber);
+console.log  (stringNumber);
+console.log (typeof(stringNumber));
+
